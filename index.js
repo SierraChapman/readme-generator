151373c37e1ce622ee7dad39529791213af96ed4
Sierra Chapman <siechap@gmail.com>
2020-07-09 20:54:38 -0700

    use title input in README

--- index.js
+++ index.js
@@ -3,36 +3,49 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // Console.log introductory message
+console.log("Welcome to the readme generator.")
 
 // Prompt user to input information using inquirer
-
-// Construct README string from input using string literals
-let readmeString = "# Test File";
-
-// Choose and create output location
-// output-n where n is the minimum integer that is greater than the rest 
-let outputDir = "output-0";
-
-// See what files are in current directory
-fs.readdir(".", (err, fileArray) => {
-    if (err) throw err;
-
-    fileArray.forEach(fileName => {
-        // If the file has a larger number than outputDir, make outputDir larger
-        if (fileName.slice(0, 7) === "output-" && fileName.slice(7) >= outputDir.slice(7)) {
-            outputDir = "output-" + (parseInt(fileName.slice(7)) + 1);
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the name of the project?",
+            name: "title"
         }
-    });
+    ])
+    .then(answers => {
+        // Construct README string from input using string literals
+        let readmeString = `# ${answers.title}`;
 
-    fs.mkdir(outputDir, err => {
-        if (err) throw err;
+        // Choose and create output location
+        // output-n where n is the minimum integer that is greater than the rest 
+        let outputDir = "output-0";
 
-            // Write string to README file using fs
-            fs.writeFile(outputDir + "/README.md", readmeString, err => {
+        // See what files are in current directory
+        fs.readdir(".", (err, fileArray) => {
+            if (err) throw err;
+
+            fileArray.forEach(fileName => {
+                // If the file has a larger number than outputDir, make outputDir larger
+                if (fileName.slice(0, 7) === "output-" && fileName.slice(7) >= outputDir.slice(7)) {
+                    outputDir = "output-" + (parseInt(fileName.slice(7)) + 1);
+                }
+            });
+
+            fs.mkdir(outputDir, err => {
                 if (err) throw err;
 
-                // Console.log success message containing output location
-                console.log("File saved at " + outputDir + "/README.md");
+                    // Write string to README file using fs
+                    fs.writeFile(outputDir + "/README.md", readmeString, err => {
+                        if (err) throw err;
+
+                        // Console.log success message containing output location
+                        console.log("File saved at " + outputDir + "/README.md");
+                });
+            });
         });
-    });
-});
+  })
+  .catch(error => {
+    throw error;
+  });
