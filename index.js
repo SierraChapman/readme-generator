863a6cb9c352e8295d03e7b3725d87de6ef063ce
Sierra Chapman <siechap@gmail.com>
2020-07-09 20:40:12 -0700

    choose output directory names

--- index.js
+++ index.js
@@ -10,16 +10,29 @@ const inquirer = require("inquirer");
 let readmeString = "# Test File";
 
 // Choose and create output location
-const outputDir = "output";
+// output-n where n is the minimum integer that is greater than the rest 
+let outputDir = "output-0";
 
-fs.mkdir(outputDir, err => {
+// See what files are in current directory
+fs.readdir(".", (err, fileArray) => {
     if (err) throw err;
 
-    // Write string to README file using fs
-    fs.writeFile(outputDir + "/README.md", readmeString, err => {
+    fileArray.forEach(fileName => {
+        // If the file has a larger number than outputDir, make outputDir larger
+        if (fileName.slice(0, 7) === "output-" && fileName.slice(7) >= outputDir.slice(7)) {
+            outputDir = "output-" + (parseInt(fileName.slice(7)) + 1);
+        }
+    });
+
+    fs.mkdir(outputDir, err => {
         if (err) throw err;
 
-        // Console.log success message containing output location
-        console.log("File saved at " + outputDir + "/README.md");
+            // Write string to README file using fs
+            fs.writeFile(outputDir + "/README.md", readmeString, err => {
+                if (err) throw err;
+
+                // Console.log success message containing output location
+                console.log("File saved at " + outputDir + "/README.md");
+        });
     });
 });
