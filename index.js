9eb48a9164421a04551729a4c1c971a40e830b7b
Sierra Chapman <siechap@gmail.com>
2020-07-09 18:01:42 -0700

    high-level psuedocode

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Import libraries using require
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+// Console log introductory message
+
+// Prompt user to input information using inquirer
+
+// Construct README string from input using string literals
+
+// Determine output location
+
+// Write string to README file using fs
+
+// Console.log success message containing output location
